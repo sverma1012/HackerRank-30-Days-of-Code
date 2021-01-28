906ee0424fddbdd6df60655676819e5093fb5064
Sneha Verma <sverma1012@gmail.com>
2021-01-28 14:45:43 -0600

    Complete Day 2

--- Day 2: Operators.py
+++ Day 2: Operators.py
@@ -2,3 +2,25 @@
 
 # Goal: Operators
 
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    meal_tip = meal_cost + (meal_cost * (tip_percent / 100))
+    meal_tip_tax = meal_tip + (meal_cost * (tax_percent / 100))
+    print(round(meal_tip_tax))
+
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
