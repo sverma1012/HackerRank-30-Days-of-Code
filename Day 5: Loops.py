243333e01b2d4038ce40759a2f01e9b6045377f7
Sneha Verma <sverma1012@gmail.com>
2021-02-01 18:17:13 -0600

    Complete Day 5

---
+++ Day 5: Loops.py
@@ -0,0 +1,13 @@
+# Goal: Loops
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+
+    for i in range(1, 11):
+        print(n, 'x', i, '=', i * n)
